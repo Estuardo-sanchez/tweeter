697425825684364dd14b5fc4375b34a03a6e271e
Estuardo <estuardo_101@hotmail.com>
2022-05-23 22:10:52 -0400

    fetch tweets using ajax GET request and show time passed since a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,47 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  $('form').submit(function(event) {
+$(document).ready(function () {
+  $('form').submit(function (event) {
     event.preventDefault()
     const form = $(this)
     const text = form.serialize();
-    $.ajax ({
+    $.ajax({
       method: 'POST',
       url: '/tweets',
       data: text
     })
-    .then(x => console.log(x))
-    // console.log(this)
-    // console.log("sucess");
+      .then((res) => {
+        const tweetVal = createTweetElement(res);
+  $('.tweet-main-container').prepend(tweetVal)
+      })
   })
 
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      data: "data"
+    })
+      .done(function (data) {
+        console.log(data)
+        renderTweets(data)
+      });
   }
-]
 
 const renderTweets = function(tweets) {
 // loops through tweets
@@ -52,7 +38,7 @@ const renderTweets = function(tweets) {
 // takes return value and appends it to the tweets container
 for (const tweet of tweets) {
   const tweetVal = createTweetElement(tweet);
-  $('.tweet-container').append(tweetVal)
+  $('.tweet-main-container').append(tweetVal)
 }
 };
 
@@ -69,7 +55,7 @@ let $tweet = `<article class="tweet-container">
 </header>
 <p class="tweet-content">${tweet.content.text}</p>
 <footer>
-  <p class="date-of-tweet">${tweet.content.created_at}</p>
+  <p class="date-of-tweet">${timeago.format(tweet.created_at)}</p>
   <div class="icons">
     <i class="flag fa-solid fa-flag"></i>
     <i class="retweet fa-solid fa-retweet"></i>
@@ -80,5 +66,5 @@ let $tweet = `<article class="tweet-container">
 return $tweet;
 };
 
-renderTweets(data);
+loadTweets();
 });
